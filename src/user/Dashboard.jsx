8f32c426d089a0fddb3463b52ed477eb1de5c6fc
rahuls-pravaah <rahuls@pravaahconsulting.com>
2025-08-30 23:01:14 +0530

    Refactor confirmation modal in Dashboard component for improved readability and user experience; enhance button structure and styling

--- src/user/Dashboard.jsx
+++ src/user/Dashboard.jsx
@@ -66,19 +66,32 @@ function Dashboard() {
     }
   };
 
-  const handleOpenConfirm = (id, title) =>{
+  const handleOpenConfirm = (id, title) => {
     openModal(
       <div>
         <h2 className="text-xl font-bold">Confirm Action</h2>
         <p className="mt-2">Are you sure you want to delete this bookamrk?</p>
         <p className="mt-2 text-blue-600 font-bold">{title}</p>
         <div className="flex justify-center items-center gap-10 mt-4 ">
-          <button onClick={()=>{deleteBookmarkHandler(id); openModal(null)}} className="p-2 hover:bg-red-600 text-red-600 hover:text-white font-bold border rounded-lg hover:cursor-pointer">Yes</button>
-          <button onClick={()=>openModal(null)} className="p-2 hover:bg-blue-600 text-blue-600 hover:text-white font-bold border rounded-lg hover:cursor-pointer">Cancel</button>
+          <button
+            onClick={() => {
+              deleteBookmarkHandler(id);
+              openModal(null);
+            }}
+            className="p-2 hover:bg-red-600 text-red-600 hover:text-white font-bold border rounded-lg hover:cursor-pointer"
+          >
+            Yes
+          </button>
+          <button
+            onClick={() => openModal(null)}
+            className="p-2 hover:bg-blue-600 text-blue-600 hover:text-white font-bold border rounded-lg hover:cursor-pointer"
+          >
+            Cancel
+          </button>
         </div>
       </div>
-    )
-  }
+    );
+  };
 
   useEffect(() => {
     if (!user) navigate("/login");
@@ -163,16 +176,41 @@ function Dashboard() {
               </div>
               <div className="flex gap-2">
                 <button
-                  className="hover:cursor-pointer text-blue-600 hover:bg-blue-700 hover:text-white border rounded-md p-1"
+                  className="flex items-center hover:cursor-pointer text-blue-600 hover:bg-blue-700 hover:text-white border rounded-md p-1"
                   onClick={() => editHandler(data.id, data.title, data.url)}
                 >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                    <path
+                      fillRule="evenodd"
+                      d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
                   Edit
                 </button>
                 <button
-                  className="hover:cursor-pointer hover:bg-red-700 hover:text-white text-red-600 border rounded-md p-1"
+                  className="flex items-center hover:cursor-pointer hover:bg-red-700 hover:text-white text-red-600 border rounded-md p-1"
                   // onClick={() => {deleteBookmarkHandler(data.id);}}
-                  onClick={()=>handleOpenConfirm(data.id, data.title)}
+                  onClick={() => handleOpenConfirm(data.id, data.title)}
                 >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
                   Delete
                 </button>
               </div>
